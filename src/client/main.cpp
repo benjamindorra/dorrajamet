64c5a0afd47136fde291bb0cbf4f8484a24b3f6d
Romain-Jamet <romain.jamet@ensea.fr>
2018-09-18 16:43:01 +0200

    Test program

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
-
-// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
-void testSFML() {
-    sf::Texture texture;
-}
 
-// Fin test SFML
 
-#include "state.h"
-
-using namespace std;
-using namespace state;
-
-int main(int argc,char* argv[]) 
+int main()
 {
-    Exemple exemple;
-    exemple.setX(53);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    cout << "It works !" << endl;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
