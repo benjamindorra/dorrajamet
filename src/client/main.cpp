8dc9893dfed722e607e7a51e34c2609d05fddcaa
benjamindorra <benjamin.dorra@yahoo.fr>
2018-11-06 17:05:05 +0000

    add a render test

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,10 +4,24 @@
 #include "../shared/json.hpp"
 #include "../shared/tools.hpp"
 #include "../shared/state.h"
+#include "render.h"
 
 void debug(int c)
 {
     std::cout << "Jusqu'ici tout va bien : " << c << std::endl;
+}
+
+void testRender(){
+    // create the window
+    render::Render render1(800,600);
+    //create the map image
+    render::Image map(400, 400);
+    //create the viewmap
+    render::ViewMap viewmap(&render1, map, sf::Vector2f(200.f, 200.f), sf::Vector2f(200.f,200.f)); 
+    //tell render to draw the viewmap
+    render1.render::Render::addToDraw("ViewMap",&viewmap);
+    //start the renderloop
+    render1.render::Render::renderLoop();
 }
 
 int main(int argc, char ** argv)
@@ -18,6 +32,10 @@ int main(int argc, char ** argv)
         {
             state::GameState testGameState("./res/testGameState.json");
             testGameState.debug();
+        }
+        else if(argv[1] == std::string("render"))
+        {
+            testRender();
         }
         else
             std::cout << "Unknown parameter: " << argv[1] << std::endl;
