b02012267b4a39d8ec654e9fc9463ab9071670b8
benjamindorra <benjamin.dorra@yahoo.fr>
2018-11-07 10:19:35 +0000

    update ViewMap.cpp

--- src/client/render/ViewMap.cpp
+++ src/client/render/ViewMap.cpp
@@ -5,21 +5,21 @@ namespace render {
     ViewMap::ViewMap () {
         
     }
-    ViewMap::ViewMap (Render * render, Image map) : map(map){
-        this->render=render;
+    ViewMap::ViewMap (Render * mainRender, Image map) : map(map){
+        this->mainRender=mainRender;
     }
-    ViewMap::ViewMap (Render * render, Image map, sf::Vector2f size, sf::Vector2f center) : map(map),size(size), center(center) {
+    ViewMap::ViewMap (Render * mainRender, Image map, sf::Vector2f size, sf::Vector2f center) : map(map),size(size), center(center) {
                 view.setCenter(center);
                 view.setSize(size);
-                this->render=render;
+                this->mainRender=mainRender;
     }
     ViewMap::~ViewMap () {}
     void ViewMap::changeSize (int var) {}
     void ViewMap::moveView (int deltaX, int deltaY) {}
     void ViewMap::draw() {
-        render->getWindow()->setView(view);
-        render->Render::getWindow()->draw(map.Image::getSprite());
-        render->Render::getWindow()->setView(render->Render::getWindow()->getDefaultView());
+        mainRender->getWindow()->setView(view);
+        mainRender->Render::getWindow()->draw(map.Image::getSprite());
+        mainRender->Render::getWindow()->setView(mainRender->Render::getWindow()->getDefaultView());
     }
 
 }
