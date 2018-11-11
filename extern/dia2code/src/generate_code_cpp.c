c68e5fd3acbaecbe781ff189264557532eaaf462
benjamindorra <benjamin.dorra@yahoo.fr>
2018-11-11 16:29:24 +0000

    update dia for sf::Image

--- extern/dia2code/src/generate_code_cpp.c
+++ extern/dia2code/src/generate_code_cpp.c
@@ -791,6 +791,7 @@ void print_include_stdlib(struct stdlib_includes* si,char* name) {
        if (!si->sfmlGraphics 
        && (strstr(name,"sf::RenderWindow")
        ||  strstr(name,"sf::VertexArray")
+       ||  strstr(name,"sf::Image")
        ||  strstr(name,"sf::Texture"))) {
            print ("#include <SFML/Graphics.hpp>\n");
            si->sfmlGraphics = 1;
