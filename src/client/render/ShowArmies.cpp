b140219f5ddf8ba027c8bc4817990ae62ec93114
benjamindorra <benjamin.dorra@yahoo.fr>
2019-01-07 13:26:14 +0000

    fixed army movement

--- src/client/render/ShowArmies.cpp
+++ src/client/render/ShowArmies.cpp
@@ -67,8 +67,10 @@ namespace render {
     }
 
     void ShowArmies::deleteArmies(){
-        for (ShowArmy * army : armies) {
-            delete army;
+        for (int i=0;i<armies.size();i++) {
+            std::cout<<armies[i]<<std::endl;
+            delete armies[i];
+            armies.erase(armies.begin()+i);
         }
     }
 }
